05ae86f32aa634171a1b149fd4b194deb1a8f316
Arpit Bharti <arpitbharti73@gmail.com>
2021-11-27 03:00:24 +0530

    Added basic next-auth authentication workflow for github

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,39 @@
 import 'public/styles/font.css'
 import 'public/styles/global.css'
 
-const MyApp = ({ Component, pageProps }) => {
+import Link from 'next/link'
+import { SessionProvider, signIn, signOut, useSession } from 'next-auth/react'
+
+export function Test() {
+  const { data: sessions, status } = useSession()
+  console.log({ sessions, status })
+
   return (
-    <div className='w-full h-full'>
-      <main>
-        <Component {...pageProps} />
-      </main>
+    <div className='flex flex-col'>
+      <Link href='/hello'>
+        <a>hello</a>
+      </Link>
+      <Link href='/'>
+        <a>Home</a>
+      </Link>
+      <button onClick={() => signIn('github')}>Signin</button>
+      <button onClick={() => signOut()}>signout</button>
     </div>
+  )
+}
+
+const MyApp = ({ Component, pageProps }) => {
+  const { session } = pageProps
+
+  return (
+    <SessionProvider session={session}>
+      <div className='w-full h-full'>
+        <main>
+          <Component {...pageProps} />
+          <Test />
+        </main>
+      </div>
+    </SessionProvider>
   )
 }
 
