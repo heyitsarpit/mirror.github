33ab13ef0be8860dedbfd72b4c0678aa0741e477
Arpit Bharti <arpitbharti73@gmail.com>
2021-11-28 03:58:34 +0530

    Added graphql client to fetch data with getServerSide props

--- pages/[user]/index.tsx
+++ pages/[user]/index.tsx
@@ -1,3 +1,33 @@
-export default function User() {
-  return <div>Users</div>
+import { gql } from 'graphql-request'
+import { githubClient } from 'lib/graphql_client'
+import { GetServerSideProps } from 'next'
+
+const USER_DATA = gql`
+  query ($login: String!) {
+    user(login: $login) {
+      login
+      bio
+      company
+    }
+  }
+`
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const user = ctx.query.user
+
+  try {
+    const data = await githubClient.request(USER_DATA, { login: user })
+
+    return { props: { data } }
+  } catch {
+    return { redirect: { permanent: false, destination: '/404' }, props: {} }
+  }
+}
+
+export default function User(props) {
+  const {
+    data: { user }
+  } = props
+
+  return <div>{user.login}</div>
 }
--- pages/hello.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div className='max-w-[75ch] mx-auto pt-12 pb-28 px-5'>Hello</div>
-}
