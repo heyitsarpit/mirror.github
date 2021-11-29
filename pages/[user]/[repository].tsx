53d8b740cafcc77fe07341294b0bcb31e2cc3489
Arpit Bharti <arpitbharti73@gmail.com>
2021-11-29 20:39:28 +0530

    Updated Header UI

--- pages/[user]/[repository].tsx
+++ pages/[user]/[repository].tsx
@@ -1,3 +1,12 @@
+import { useRouter } from 'next/router'
+
 export default function Repository() {
-  return <div>Repository</div>
+  const router = useRouter()
+  console.log(router)
+
+  if (!router.query.user) {
+    return <div>No user Found</div>
+  }
+
+  return <div className='max-w-[75ch] h-full mx-auto pt-12 pb-28 px-5'>Repository</div>
 }
--- ui/Header.tsx
+++ ui/Header.tsx
@@ -1,9 +1,28 @@
+import Link from 'next/link'
+
 import { UserDropDown } from './UserDropDown'
+
+function MirrorLogo() {
+  return (
+    <Link href='/'>
+      <a
+        className='flex items-center justify-center w-12 h-12 p-4 bg-gray-300 rounded-full'
+        title='home'>
+        <img src='/images/mirror.png' alt='Github Mirror' className='w-full' />
+      </a>
+    </Link>
+  )
+}
 
 export function Header() {
   return (
-    <header className='flex flex-row-reverse p-4'>
-      <UserDropDown />
+    <header className='relative w-full h-16 mb-20'>
+      <div className='fixed h-16 z-40 w-full flex justify-between backdrop-blur-[20px] backdrop-saturate-150 bg-white/50 dark:bg-[#0D0D1050]'>
+        <nav className='w-full sm:max-w-[75ch] m-auto flex px-5 justify-between items-center '>
+          <MirrorLogo />
+          <UserDropDown />
+        </nav>
+      </div>
     </header>
   )
 }
